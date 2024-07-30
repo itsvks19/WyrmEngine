53f3310e284d4c7e6bb1696c16d348bc6eb8d307
Vivek <itsvks19@gmail.com>
2024-07-30 22:14:48 +0530

    refactor: remove unused imports and SDK versions
    Removes unused imports from WyrmApplication.java and removes compileSdk, minSdk, and targetSdk from file
    tree/build.gradle.

--- app/src/main/java/com/wyrm/engine/WyrmApplication.java
+++ app/src/main/java/com/wyrm/engine/WyrmApplication.java
@@ -2,7 +2,6 @@ package com.wyrm.engine;
 
 import android.app.Application;
 
-import com.blankj.utilcode.util.AppUtils;
 import com.wyrm.engine.core.Core;
 
 public class WyrmApplication extends Application {
