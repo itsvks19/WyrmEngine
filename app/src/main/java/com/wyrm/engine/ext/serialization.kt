c57492fa3d84b9bd0f7396e7896102edb8f32153
Vivek <itsvks19@gmail.com>
2024-07-30 23:23:01 +0530

    feat: add toJson and fromJson extension functions
    Adds `toJson` and `fromJson` extension functions for easy serialization and deserialization of objects using Gson.
    
    - `toJson`: Converts any object to a JSON string.
    - `fromJson`: Converts a JSON string to an object of the specified type.
    
    These functions now also require the objects to implement the `Serializable` interface.

--- app/src/main/java/com/wyrm/engine/ext/serialization.kt
+++ app/src/main/java/com/wyrm/engine/ext/serialization.kt
@@ -1,11 +1,12 @@
 package com.wyrm.engine.ext
 
 import com.google.gson.Gson
+import java.io.Serializable
 
-fun Any.toJson(): String {
+fun <T : Serializable> T.toJson(): String {
   return Gson().toJson(this)
 }
 
-inline fun <reified T> String.fromJson(): T {
+inline fun <reified T : Serializable> String.fromJson(): T {
   return Gson().fromJson(this, T::class.java)
 }
