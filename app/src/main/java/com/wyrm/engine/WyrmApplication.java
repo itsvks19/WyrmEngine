c505c9a54590f6d9251e219ff7bd4197fd9dd5f8
Vivek <itsvks19@gmail.com>
2024-08-05 11:55:37 +0530

    feat: destroy core when no activities are present
    Registers an activity lifecycle callback to track the number of created activities. When the count reaches zero, the core is
     destroyed.
    This ensures the core is properly destroyed when the application is no longer in use.

--- app/src/main/java/com/wyrm/engine/WyrmApplication.java
+++ app/src/main/java/com/wyrm/engine/WyrmApplication.java
@@ -1,13 +1,61 @@
 package com.wyrm.engine;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.wyrm.engine.core.Core;
 
 public class WyrmApplication extends Application {
+  private int counterActivityCreate = 0;
+
   @Override
   public void onCreate() {
     super.onCreate();
     Core.getInstance().onStart(getApplicationContext());
+
+    registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+      @Override
+      public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+        counterActivityCreate++;
+      }
+
+      @Override
+      public void onActivityStarted(@NonNull Activity activity) {
+      }
+
+      @Override
+      public void onActivityResumed(@NonNull Activity activity) {
+      }
+
+      @Override
+      public void onActivityPaused(@NonNull Activity activity) {
+      }
+
+      @Override
+      public void onActivityStopped(@NonNull Activity activity) {
+      }
+
+      @Override
+      public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+      }
+
+      @Override
+      public void onActivityDestroyed(@NonNull Activity activity) {
+        counterActivityCreate--;
+        if (counterActivityCreate == 0) {
+          Core.getInstance().destroyCore();
+        }
+      }
+    });
+  }
+
+  @Override
+  public void onTerminate() {
+    super.onTerminate();
+    Core.getInstance().destroy();
   }
 }
