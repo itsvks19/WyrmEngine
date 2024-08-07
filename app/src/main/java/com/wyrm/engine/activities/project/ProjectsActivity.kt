2d1090dd79ad83e7266161cc9d96ec435a87c181
Vivek <itsvks19@gmail.com>
2024-08-07 17:23:27 +0530

    feat: add loading progress bar to projects list
    Adds a loading progress bar to the projects list activity. This progress bar is displayed while the projects are being loaded
     from storage.

--- app/src/main/java/com/wyrm/engine/activities/project/ProjectsActivity.kt
+++ app/src/main/java/com/wyrm/engine/activities/project/ProjectsActivity.kt
@@ -12,17 +12,33 @@ import com.wyrm.engine.adapters.project.ProjectsAdapter
 import com.wyrm.engine.databinding.ActivityProjectsBinding
 import com.wyrm.engine.ext.open
 import com.wyrm.engine.managers.ProjectManager
+import com.wyrm.engine.model.project.Project
 import com.wyrm.engine.ui.dialog.WyrmDialog
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ProjectsActivity : BaseActivity<ActivityProjectsBinding>(ActivityProjectsBinding::inflate) {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    binding.loadingProgress.show()
+  }
 
+  override fun onStart() {
+    super.onStart()
     showEulaDialogIfRequired {
       binding.projectsList.apply {
         setHasFixedSize(true)
-        layoutManager = GridLayoutManager(this@ProjectsActivity, 4)
-        adapter = ProjectsAdapter(ProjectManager.instance.projects.toMutableList())
+        var projects = mutableListOf<Project>()
+        CoroutineScope(Dispatchers.IO).launch {
+          projects = ProjectManager.instance.projects.toMutableList()
+        }.invokeOnCompletion {
+          CoroutineScope(Dispatchers.Main).launch {
+            layoutManager = GridLayoutManager(this@ProjectsActivity, 4)
+            adapter = ProjectsAdapter(projects)
+            binding.loadingProgress.hide()
+          }
+        }
       }
 
       binding.addProject.setOnClickListener { open(NewProjectActivity::class.java) }
